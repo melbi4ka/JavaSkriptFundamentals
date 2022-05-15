87dcd487e51cba81ffc43893d7a3a3bc644564dd
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-05-15 13:57:44 +0300

    Add files via upload

---
+++ basicSyntaxConditionalStatementsAndLoops/login.js
@@ -0,0 +1,28 @@
+function login (input){
+    let passCounter = 0;
+    let password = input[0];
+
+    for (i = 0; i< input.length -1; i++){
+        
+        let reverse = input[i+1].split("").reverse().join("");
+        passCounter++;
+
+        if (password === reverse){
+            console.log(`User ${password} logged in.`);
+            //break; 
+        } else {
+            
+            if (passCounter < 4){
+                console.log("Incorrect password. Try again.");
+
+            } else if (passCounter == 4) { 
+                console.log(`User ${password} blocked!`);
+                break; 
+            }
+        }
+    }
+}
+
+login (['Acer','login','go','let me in','recA'])
+login(['momo','omom'])
+login(['sunny','rainy','cloudy','sunny','not sunny'])
