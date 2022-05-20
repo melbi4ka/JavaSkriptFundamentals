239a55457b9632f736c7f9e9edf335da84690b4a
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-05-20 19:37:23 +0300

    Add files via upload

---
+++ dataTypesAndVariables/charsToString.js
@@ -0,0 +1,16 @@
+function charToString (charOne, charTwo, charThree) {
+    console.log(charOne + charTwo + charThree)
+}
+
+charToString('a',
+'b',
+'c'
+)
+charToString('%',
+'2',
+'o'
+) 
+charToString('1',
+'5',
+'p'
+)
