b4fc26e893d5e2878243ccd9431e48f2503eba4b
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-05-20 19:38:46 +0300

    Update charsToString.js

--- dataTypesAndVariables/charsToString.js
+++ dataTypesAndVariables/charsToString.js
@@ -1,16 +1,3 @@
 function charToString (charOne, charTwo, charThree) {
     console.log(charOne + charTwo + charThree)
 }
-
-charToString('a',
-'b',
-'c'
-)
-charToString('%',
-'2',
-'o'
-) 
-charToString('1',
-'5',
-'p'
-)
