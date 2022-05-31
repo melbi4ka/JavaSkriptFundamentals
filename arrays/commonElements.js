07a4a0f8c4018926647e6cb06daa76625b740d3e
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-05-31 22:40:04 +0300

    Update commonElements.js

--- arrays/commonElements.js
+++ arrays/commonElements.js
@@ -7,8 +7,3 @@ function commonElements (arrOne, arrTwo){
         }
     }
 }
-commonElements(['Hey', 'hello', 2, 4, 'Peter', 'e'],
-['Petar', 10, 'hey', 4, 'hello', '2'])
-
-commonElements(['S', 'o', 'f', 't', 'U', 'n', 'i', ' '],
-['s', 'o', 'c', 'i', 'a', 'l'])
