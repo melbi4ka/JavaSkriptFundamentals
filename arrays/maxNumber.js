39de5bf3be9678793b1826b6c30594a6b7aa9ddb
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-05-31 22:41:25 +0300

    Update maxNumber.js

--- arrays/maxNumber.js
+++ arrays/maxNumber.js
@@ -11,8 +11,3 @@ function maxNumber (arr){
     }
     console.log(max)
 }
-
-maxNumber([1, 4, 3, 2])
-maxNumber([14, 24, 3, 19, 15, 17])
-maxNumber([41, 41, 34, 20])
-maxNumber([27, 19, 42, 2, 13, 45, 48])
