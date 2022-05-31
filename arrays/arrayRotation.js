4a74d08635961e0fbe1cac24bffd8c779cdc40f0
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-05-31 22:39:41 +0300

    Update arrayRotation.js

--- arrays/arrayRotation.js
+++ arrays/arrayRotation.js
@@ -9,10 +9,3 @@ function arrayRotation (arr, number){
     }    
     console.log(arr.join(" "))
 }
-
-
-arrayRotation([51, 47, 32, 61, 21], 2)
-console.log("------")
-arrayRotation([32, 21, 61, 1], 4)
-console.log("------")
-arrayRotation([2, 4, 15, 31], 5)
