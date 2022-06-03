9385b24d55b968beb5edfac2d46aee9aaf328d05
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-06-03 14:51:13 +0300

    Update mergeArrays.js

--- arrays/mergeArrays.js
+++ arrays/mergeArrays.js
@@ -10,7 +10,3 @@ function mergeArrays (arrOne, arrTwo) {
     }
     console.log(arrTree.join(" - "))
 }
-mergeArrays(['5', '15', '23', '56', '35'],
-['17', '22', '87', '36', '11'])
-mergeArrays(['13', '12312', '5', '77', '4'],
-['22', '333', '5', '122', '44'])
