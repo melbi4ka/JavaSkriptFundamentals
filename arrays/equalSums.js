4ce58fa2fc67503052f67aaad829f98e8504efdc
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-06-06 22:00:33 +0300

    Update equalSums.js

--- arrays/equalSums.js
+++ arrays/equalSums.js
@@ -22,9 +22,3 @@ function equalSums (arr){
         console.log("no")
     }
 }
-
-equalSums([1, 2, 3, 3])
-//equalSums([[1, 2]])
-//equalSums([1])
-//equalSums([1, 2, 3])
-//equalSums([10, 5, 5, 99, 3, 4, 2, 5, 1, 1, 4])
