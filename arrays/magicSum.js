424cf137f143831de61a5624bf6be6d79aba208d
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-09-15 16:12:33 +0300

    Add files via upload

---
+++ arrays/magicSum.js
@@ -0,0 +1,28 @@
+function magicSum(arr, number){
+
+    let pairs = []
+
+    for (let i = 0; i < arr.length; i++){
+        for (let j = i+1; j < arr.length; j++){
+            if (arr[i] + arr[j] === number){
+                pairs.push(arr[i])
+                pairs.push(arr[j])
+                console.log(pairs.join(" "))
+                pairs = []
+            }
+        }
+    }
+}
+
+
+
+magicSum([1, 7, 6, 2, 19, 23], 
+    8
+    )
+magicSum([14, 20, 60, 13, 7, 19, 8], 
+    27
+    )
+magicSum([1, 2, 3, 4, 5, 6],
+    6
+    )
+
