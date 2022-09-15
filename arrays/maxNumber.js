3c8eff149d22cb2295b9626dc9678a96c5d137cc
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-09-15 16:12:16 +0300

    Add files via upload

--- arrays/maxNumber.js
+++ arrays/maxNumber.js
@@ -2,12 +2,24 @@ function maxNumber (arr){
     let max = []
 
     for (let i = 0; i < arr.length; i++){
-        if (arr[i] > arr[i+1]){
-            max.push(arr[i])
+        let current = arr[i];
+        let isTopInteger = true;
+        for (let j = i+1; j < arr.length; j++){
+            let nextEl = arr[j];
+            if (current <= nextEl){
+                isTopInteger = false;
+                break;
+            }
         }
-        if (i == arr.length-1){
-            max.push(arr[i])
+
+        if (isTopInteger){
+            max.push(current)
         }
     }
-    console.log(max)
+    console.log(max.join(" "))
 }
+
+maxNumber([1, 4, 3, 2])
+maxNumber([14, 24, 3, 19, 15, 17])
+maxNumber([41, 41, 34, 20])
+maxNumber([27, 19, 42, 2, 13, 45, 48])
